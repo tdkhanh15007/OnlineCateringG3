0aeb3cdb676e49d22e4e8c027d2bfc3768f1e5d2
tdkhanh15007 <34757823+tdkhanh15007@users.noreply.github.com>
2018-03-20 13:28:30 +0700

    ver 1.9

---
+++ OCatering-war/src/java/controller/SuppManagedBean.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import MyEntity.District;
+import MyEntity.Supplier;
+import MySsbean.DistrictFacadeLocal;
+import MySsbean.SupplierFacadeLocal;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Khanh
+ */
+@ManagedBean
+@RequestScoped
+public class SuppManagedBean {
+    @EJB
+    private DistrictFacadeLocal districtFacade;
+    @EJB
+    private SupplierFacadeLocal supplierFacade;
+
+    int supID,distID;
+    String name,address,phone,mess="";
+    boolean status;
+
+    public int getSupID() {
+        return supID;
+    }
+
+    public void setSupID(int supID) {
+        this.supID = supID;
+    }
+
+    public int getDistID() {
+        return distID;
+    }
+
+    public void setDistID(int distID) {
+        this.distID = distID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMess() {
+        return mess;
+    }
+
+    public void setMess(String mess) {
+        this.mess = mess;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    public List<District> listD(){
+        return districtFacade.findAll();
+    }
+    
+    /**
+     * Creates a new instance of SuppManagedBean
+     */
+    public SuppManagedBean() {
+    }
+    
+    public List<Supplier> listAll(){
+        return supplierFacade.findAll();
+    }
+    
+    public String findSupp(int id){
+        Supplier sp = supplierFacade.find(id);
+        setName(sp.getName());
+        setAddress(sp.getAddress());
+        setPhone(sp.getPhone());
+        setDistID(sp.getDistrictId().getDistrictId());
+        setStatus(sp.getStatus());
+        return "updatesupp";
+    }
+    
+    public String updateSupp(int id){
+        try{
+            Supplier sp = supplierFacade.find(id);
+            District ds = districtFacade.find(distID);
+            sp.setName(name);
+            sp.setPhone(phone);
+            sp.setAddress(address);
+            sp.setDistrictId(ds);
+            sp.setStatus(status);
+            supplierFacade.edit(sp);
+            return "supplier";
+        }catch(Exception e){
+            return "updatesupp";
+        }
+    }
+    
+    public String createSupp(){
+        try{
+            
+            return "supplier";
+        }catch(Exception e){
+            return "createsupp";
+        }
+    }
+}
